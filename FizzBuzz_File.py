09b56734e000895fb093cc923898cc5d46a9c290
ph2729-bit <ph2729@columbia.edu>
2026-02-27 21:24:15 -0500

    FizzBuzz

--- FizzBuzz_File.py
+++ FizzBuzz_File.py
@@ -1,5 +1,16 @@
 import numpy as np
 
 def FizzBuzz(start, finish):
-    v = ['buzz', 41, 'fizz', 43, 44, 'fizzbuzz']
-    return(v)
+    nums = np.arange(start, finish + 1)
+    out = np.array(nums, dtype = object)
+
+    mod3 = nums % 3 == 0
+    mod5 = nums % 5 == 0
+    mod15 = nums % 15 == 0
+
+    out[mod3] = 'fizz'
+    out[mod5] = 'buzz'
+    out[mod15] = 'fizzbuzz'
+
+    return list(out)
+
