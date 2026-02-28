ec2bd0808abfa15b66452d738139eda28a48e6c6
ph2729-bit <ph2729@columbia.edu>
2026-02-27 20:46:14 -0500

    code

--- BondPrice_File.py
+++ BondPrice_File.py
@@ -1,8 +1,14 @@
 import numpy as np
 
 def getBondPrice(y, face, couponRate, m, ppy=1):
-    if ppy == 1:
-        x = 2170604
-    if ppy == 2:
-        x = 2171686
-    return(x)
+    y /= ppy
+    couponRate /= ppy
+    m *= ppy
+    cf = face * couponRate
+    t = np.arange(1, m + 1)
+    discount = (1 + y) ** (-t)
+
+    pvcfsum = (cf * discount).sum() + face * discount[-1]
+    return pvcfsum
+
+
